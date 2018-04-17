a811d02c9ac008280b25fc097d5d135359ed345f
fang0975 <happy881686@gmail.com>
2018-04-17 23:34:57 +0800

    First commit
    BMI and BMR Layout
    MainActivity BMI Button and BMR button

---
+++ app/src/main/java/com/example/honor/myhealthlife/CalculateBMRActivity.java
@@ -0,0 +1,8 @@
+package com.example.honor.myhealthlife;
+
+/**
+ * Created by Honor on 2018/4/17.
+ */
+
+public class CalculateBMRActivity {
+}
--- app/src/main/java/com/example/honor/myhealthlife/MainActivity.java
+++ app/src/main/java/com/example/honor/myhealthlife/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.honor.myhealthlife;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button mBtnBmi, mBtnBmr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnBmi = (Button) findViewById(R.id.btnBMI);
+        mBtnBmr = (Button) findViewById(R.id.btnBMR);
+        mBtnBmi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CalculateBMIActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mBtnBmr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,CalculateBMRActivity.class));
+            }
+        });
+
     }
 }
